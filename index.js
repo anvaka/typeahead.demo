3686bfa30639325baa557741bc3d97beb3668baf
anvaka <anvaka@gmail.com>
2014-02-18 23:01:50 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = DemoCtrl;
+
+require('typeahead.an'); // we are going to use typeahead in this demo
+require('an').controller(DemoCtrl);
+require('an').flush(); // flush and bootstrap angular app
+
+function DemoCtrl($scope, $http) {
+  $scope.getLocation = function(val) {
+      return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+          address: val,
+          sensor: false
+        }
+      }).then(function(res){
+        var addresses = [];
+        angular.forEach(res.data.results, function(item){
+          addresses.push(item.formatted_address);
+        });
+        return addresses;
+      });
+    };
+}
